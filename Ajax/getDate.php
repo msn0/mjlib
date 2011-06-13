c403b249f63c158be679944d7c9c0bb2c81d7e9a
Michał Jezierski <ambinanitelo@gmail.com>
2011-06-13 11:00:16 +0200

    xhr and webworkers

--- Ajax/getDate.php
+++ Ajax/getDate.php
@@ -1,16 +1,16 @@
 <?php
 
-	date_default_timezone_set('UTC');
-	
-	//sleep(round(rand()/1000000000));
+date_default_timezone_set('UTC');
 
-	if($_GET['t'] == 0){
-		$d = getdate();
-		print_r("{\"s\":\"$d[seconds]\",\"m\":\"$d[minutes]\",\"h\":\"$d[hours]\",\"u\":\"".microtime(true)."\"}");
-	}
-	if($_GET['t'] == 1){
-		$d = time();
-		print_r("$d");
-	}
+//sleep(round(rand()/1000000000));
 
+if ($_GET['t'] == 0) {
+    $d = getdate();
+    $m = microtime(true) . "";
+    print_r("{\"s\":\"$d[seconds]\",\"m\":\"$d[minutes]\",\"h\":\"$d[hours]\",\"u\":\"." . $m[11]. "\"}");
+}
+if ($_GET['t'] == 1) {
+    $d = time();
+    print_r("$d");
+}
 ?>
